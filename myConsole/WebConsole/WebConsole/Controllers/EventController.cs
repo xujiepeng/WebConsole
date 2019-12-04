b905cf14c74d01f9d46ba87336a1904612b06f79
yangwohenmai <zongxianyang@outlook.com>
2019-12-04 09:55:33 +0800

    图表

--- myConsole/WebConsole/WebConsole/Controllers/EventController.cs
+++ myConsole/WebConsole/WebConsole/Controllers/EventController.cs
@@ -5,11 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using WebConsole.Models.Model;
-using System.Net;
-using System.Text;
-using System.IO;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
+using WebConsole.Models.Common.HttpHelper;
 
 namespace WebConsole.Controllers
 {
@@ -18,19 +14,12 @@ namespace WebConsole.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            string str = getUrlResponse("http://data.gtimg.cn/flashdata/hushen/minute/sz000004.js?maxage=111").ToString();
-            string[] array0 = str.Split("\\n\\\n");
-            string[] arraynew = new string[array0.Length - 1];
-            for (int i = 2; i < array0.Length-2; i++)
-            {
-                arraynew[i-2] = array0[i].Split(' ')[1];
-            }
             return View();
         }
 
         public string DrawLine(string id)
         {
-            string str = getUrlResponse("http://data.gtimg.cn/flashdata/hushen/minute/sz000004.js?maxage=111").ToString();
+            string str = HttpsGet.GetUrlResponse("http://data.gtimg.cn/flashdata/hushen/minute/sz000004.js?maxage=111").ToString();
             string[] array0 = str.Split("\\n\\\n");
             string[] arraynew = new string[array0.Length - 3];
             for (int i = 2; i < array0.Length - 1; i++)
@@ -62,44 +51,6 @@ namespace WebConsole.Controllers
             return jsonData;
         }
 
-        /// <summary>
-        /// 访问安全的https网站时要SSL验证，用回调安全验证的方法，解决这个问题
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        public static StringBuilder getUrlResponse(string url)
-        {
-            HttpWebResponse resp = null;
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
-            {
-                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
-            }
 
-            resp = (HttpWebResponse)req.GetResponse();
-
-            Stream responseStream = resp.GetResponseStream();
-            // 对接响应流(以"GBK"字符集)
-            StreamReader sReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
-
-            Char[] sReaderBuffer = new Char[256];
-            int count = sReader.Read(sReaderBuffer, 0, 256);
-            StringBuilder content = new StringBuilder();
-            while (count > 0)
-            {
-                String tempStr = new String(sReaderBuffer, 0, count);
-                content.Append(tempStr);
-                count = sReader.Read(sReaderBuffer, 0, 256);
-            }
-            // 读取结束
-            sReader.Close();
-
-            return content;
-        }
-        //处理安全连接问题
-        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
-        {
-            return true; //Always accept
-        }
     }
 }
---
+++ myConsole/WebConsole/WebConsole/Models/Common/HttpHelper/HttpGet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebConsole.Models.Common.HttpHelper
+{
+    public class HttpGet
+    {
+    }
+}
