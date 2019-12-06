d8c95926e40ca5d3ba145e09004d88b63f6696b6
yangwohenmai <zongxianyang@outlook.com>
2019-12-07 00:44:13 +0800

    图表

--- myConsole/WebConsole/WebConsole/Models/K_IndexViewModel.cs
+++ myConsole/WebConsole/WebConsole/Models/K_IndexViewModel.cs
@@ -10,7 +10,6 @@ namespace WebConsole.Models
     {
         public string GetData(string stockcode, string stockname)
         {
-            //string stockLink = string.Format("http://data.gtimg.cn/flashdata/hushen/minute/{0}.js?maxage=111", stockcode);
             HttpsGet httpget = new HttpsGet();
             string str = httpget.GetUrlResponse("https://www.highcharts.com/samples/data/aapl-ohlcv.json").ToString();
             return str;
